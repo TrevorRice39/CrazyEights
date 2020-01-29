1dcaac67958f653cb98c11dfac028e8b4bfadf20
Trevor Rice <trevor_rice39@mymail.eku.edu>
2020-01-29 08:42:08 -0500

    implementing the game mechanics for each move

--- Cards.py
+++ Cards.py
@@ -1,4 +1,5 @@
 import random
+from random import randint
 
 class Card:
 
@@ -25,6 +26,9 @@ class Deck:
                   "Queen", "King"]
             
     def __init__(self):
+        self.newDeck()
+        
+    def newDeck(self):
         # dictionary of all cards {card id -> card)
         self.deck_dict = {card.id:card for card in self.getDeck()}
 
@@ -32,6 +36,11 @@ class Deck:
         self.deck = [cardId for cardId in self.deck_dict]
         random.shuffle(self.deck)
         self.size = len(self.deck)
+
+    def newEmptyDeck(self):
+        self.deck_dict = dict()
+        self.deck = list()
+        self.size = 0
 
     def getDeck(self):
         for rank in self.rank_names:
@@ -43,6 +52,11 @@ class Deck:
     def removeTop(self):
         return self.deck_dict[self.deck.pop()]
 
+    def addCard(self, card):
+        self.deck_dict[card.id]  = card
+        self.deck.append(card.id)
+        self.size += 1
+
 class Hand:
 
     def __init__(self, player_number):
@@ -52,8 +66,8 @@ class Hand:
 
     def __str__(self):
         hand_str = "Player {0}'s hand: ".format(self.player_number)
-        for card in self.cards:
-            hand_str += str(card) + ", "
+        for index, card in enumerate(self.cards):
+            hand_str += str(card) + "({0}), ".format(index)
 
         return hand_str[0 : -2]
     def addCard(self, card):
@@ -70,6 +84,9 @@ class Game:
         # make the deck for the game
         self.deck = Deck()
 
+        self.discard = Deck() # discard pile
+        self.discard.newEmptyDeck() # empty deck
+
         # create the players
         self.player1 = Hand(1)
         self.player2 = Hand(2)
@@ -77,9 +94,13 @@ class Game:
         
 
     def startGame(self):
-        self.dealInitialHands()
+        self.dealInitialHands() # deal the initial hands to the 2 players
 
-        self.turn = randint(1, 3) # randomly choose player 1 or 2 to go first
+        self.turn = randint(1, 2) # randomly choose player 1 or 2 to go first
+
+        self.currentSuit = self.deck.peekTop().suit # get the suit of the top of the pile
+
+        self.discard.addCard(self.deck.removeTop())
 
     def dealInitialHands(self):
 
@@ -96,8 +117,34 @@ class Game:
         self.player1.addCard(self.deck.removeTop())
         self.player2.addCard(self.deck.removeTop())
 
-        if self.deck.peekTop().rank == "Eight":
+        if self.deck.peekTop().rank == "Eight": # restart the game if an eight is at the top
             self = Game()
 
-    def makeMove(self, playerNumber, card):
-        pass
+    # player number signifies which player is moving
+    # card is their move
+    # newSuit must be specified if they play an eight
+    def makeMove(self, playerNumber, cardIndex, newSuit):
+        card = self.deck[cardIndex]
+        if card.suit == self.currentSuit:
+            self.discard.addCard(card)
+        elif card.rank == self.discard.peekTop().rank:
+            self.discard.addCard(card)
+        elif card.rank == "Eight":
+            self.discard.addCard(card)
+            self.currentSuit = newSuit
+    
+    
+
+game = Game()
+
+game.startGame()
+
+while True:
+    print("Top of discard: {0}:".format(game.discard.peekTop()))
+    if game.turn == 1:
+        print(game.player1)
+    else:
+        print(game.player2)
+    print("Player {0}, make your move: ".format(game.turn), end = '')
+    move = input()
+    game.makeMove(game.turn, move, "")
